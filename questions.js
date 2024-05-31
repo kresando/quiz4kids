2fdef0569172658e1e48b78fa8ef42b67409db92
kresando <79419057+kresando@users.noreply.github.com>
2024-05-31 19:46:58 +0800

    quiz4kids

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Apa ibu kota Indonesia?",
+        answer: "A. Jakarta",
+        options: [
+            "A. Jakarta",
+            "B. Surabaya",
+            "C. Bandung",
+            "D. Medan",
+        ]
+    },
+    {
+        numb: 2,
+        question: "Berapa jumlah hari dalam seminggu?",
+        answer: "C. 7",
+        options: [
+            "A. 5",
+            "B. 6",
+            "C. 7",
+            "D. 8"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Apa warna bendera Indonesia?",
+        answer: "A. Merah dan putih",
+        options: [
+            "A. Merah dan putih",
+            "B. Biru dan putih",
+            "C. Hijau dan kuning",
+            "D. Merah dan hitam"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Apa yang harus kita lakukan sebelum menyeberang jalan?",
+        answer: "C. Melihat ke kiri dan kanan",
+        options: [
+            "A. Berlari cepat",
+            "B. Menyanyi",
+            "C. Melihat ke kiri dan kanan",
+            "D. Bermain ponse"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Apa nama planet yang kita tinggali?",
+        answer: "C. Bumi",
+        options: [
+            "A. Mars",
+            "B. Venus",
+            "C. Bumi",
+            "D. Jupiter"
+        ]
+    },
+]
