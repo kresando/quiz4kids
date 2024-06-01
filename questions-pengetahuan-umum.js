22602e6d70341b86430cd5dc9363dc77e9aaf4f1
kresando <79419057+kresando@users.noreply.github.com>
2024-06-01 23:05:35 +0800

    Add files via upload

---
+++ questions-pengetahuan-umum.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+            numb: 1,
+            question: "Siapa presiden pertama Indonesia?",
+            answer: "A. Soekarno",
+            options: [
+                "A. Soekarno",
+                "B. Soeharto",
+                "C. Megawati Soekarnoputri",
+                "D. Susilo Bambang Yudhoyono"
+            ]
+        },
+        {
+            numb: 2,
+            question: "Apa ibu kota Indonesia?",
+            answer: "A. Jakarta",
+            options: [
+                "A. Jakarta",
+                "B. Surabaya",
+                "C. Bandung",
+                "D. Medan"
+            ]
+        },
+        {
+            numb: 3,
+            question: "Apa bahasa resmi Indonesia?",
+            answer: "A. Bahasa Indonesia",
+            options: [
+                "A. Bahasa Indonesia",
+                "B. Bahasa Inggris",
+                "C. Bahasa Mandarin",
+                "D. Bahasa Arab"
+            ]
+        },
+        {
+            numb: 4,
+            question: "Apa mata uang Indonesia?",
+            answer: "A. Rupiah",
+            options: [
+                "A. Rupiah",
+                "B. Dolar",
+                "C. Euro",
+                "D. Yen"
+            ]
+        },
+        {
+            numb: 5,
+            question: "Apa lagu kebangsaan Indonesia?",
+            answer: "A. Indonesia Raya",
+            options: [
+                "A. Indonesia Raya",
+                "B. Maju Tak Gentar",
+                "C. Garuda Pancasila",
+                "D. Tanah Airku"
+            ]
+        },
+    ]
